e301d57bb2940c54794765cbd36941a41d3b8e30
Erki Elbrecht <erki.elbrecht@gmail.com>
2022-12-01 03:14:19 +0100

    Comments for the client file.

--- src/protocols/test_stream/client.py
+++ src/protocols/test_stream/client.py
@@ -3,9 +3,11 @@ import PySimpleGUI as sg
 import sound.emitter as em
 import sound.stream as sound_stream
 
+# A variable for storing the current status of the program. Useful for testing and debugging.
 status = ""
 
 
+# Layout for your client UI. You can copy this one to start off with.
 def get_layout():
     return [
         [sg.Text('Request file')],
@@ -15,19 +17,25 @@ def get_layout():
     ]
 
 
+# Callback function that gets executed every time an action finishes e.g. listening, emitting.
+# Pass this function to listening and emitting calls.
 def callback(header, data):
     print(header, data)
 
 
+# This function gets called when the client is first opened.
+# A good practice is to initiate the stream.
 def init():
     sound_stream.start_stream()
 
 
+# A function for setting the feedback listening and emitting plus other tasks.
 def set_status(current_status):
     global status
     status = current_status
 
 
+# Function that listens to the user input from Simple python GUI.
 def listen(event, values, window):
     window['-TEXT-'].update(status)
     if event == 'Stop':
